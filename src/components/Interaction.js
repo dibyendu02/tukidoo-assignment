d2457fc9f742470f5e501586f3ae54598a6db7b2
Dibyendu Das <get2dibyendu@gmail.com>
2023-11-23 14:54:48 +0530

    interaction changes

--- src/components/Interaction.js
+++ src/components/Interaction.js
@@ -6,6 +6,10 @@ import { FaRegShareFromSquare } from "react-icons/fa6";
 import { PiWechatLogo } from "react-icons/pi";
 import { IoExitOutline } from "react-icons/io5";
 
+import { FaFaceSmileBeam } from "react-icons/fa6";
+import { FaPlus } from "react-icons/fa6";
+import { GiSoundWaves } from "react-icons/gi";
+
 import teacher from "../images/teacher.jpg"
 import student from "../images/student.jpg"
 
@@ -20,8 +24,25 @@ const Interaction = () => {
       ];
   return (
     <div className="w-[15%] h-[70%] top-[22%] left-[10%] flex flex-col gap-5 items-center  absolute">
-        <img src={student} alt='student' className="w-48 rounded-2xl" />
-        <img src={teacher} alt='teacher' className="w-48 rounded-2xl"/>
+        <div className="relative cursor-pointer">
+            <img src={student} alt='student' className="w-48 rounded-2xl" />
+            <label className="absolute left-0 bottom-0 bg-slate-200 py-1 px-5 rounded-r-[50px] rounded-b-[50px] font-bold  ">Ridha</label>
+            <div className="absolute right-5 top-4 flex gap-2 bg-slate-200 py-1 px-2 rounded-2xl">
+                <FaFaceSmileBeam style={{color: "yellow", backgroundColor: "black", borderRadius: "18px"}}/>
+                <FaPlus/>
+            </div>
+        </div>
+
+        <div className="relative cursor-pointer">
+            <img src={teacher} alt='teacher' className="w-48 rounded-2xl"/>
+            <label className="absolute left-0 bottom-0 bg-slate-200 py-1 px-5 rounded-r-[50px] rounded-b-[50px] font-bold  ">Ms. Kaur</label>
+            {/* <GiSoundWaves style={{position: "absolute", top: "15px", left: "15px", fontSize: "30px", color: "slate"}}/> */}
+            <div className="absolute left-5 top-3 text-4xl text-slate-300">
+                <GiSoundWaves/>
+            </div>
+        </div>
+        
+        
         
         
         <div className=" flex flex-wrap gap-5 mx-5 " >
